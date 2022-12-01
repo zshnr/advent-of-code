ac45de676ebf148dbd78e515bf1413cd9e6a1787
Zeeshan Rasool <zeeshan@permutive.com>
2022-12-01 16:26:17 +0000

    reads better in pipe

--- 2022/day-01/readable-solution.ts
+++ 2022/day-01/readable-solution.ts
@@ -6,6 +6,7 @@ const split = (pattern: string | RegExp) => (s: string) => s.split(pattern)
 const flatMap = <A, B>(mapFn: (arg: A) => B) => (arr: Array<A>) => arr.flatMap(mapFn);
 const sort = <A>(compareFn: (arg1: A, arg2: A) => number) => (arr: Array<A>) => [...arr.sort(compareFn)]
 const slice = <T>(from: number, to?: number) => (arr: Array<T>) => to ? arr.slice(from, to) : arr.slice(from)
+const reduce = <T>(reduceFn: (accumulator: T, current: T) => T, initialValue: T) => (arr: Array<T>) => arr.reduce(reduceFn, initialValue)
 
 const readInput = async (): Promise<string> => {
   return await fs.readFile(`${__dirname}/input.txt`, { encoding: 'utf-8' });
@@ -21,7 +22,7 @@ const calculateTotalCaloriesForEachElf = (calories: Array<number>): number =>
 
 const sortDescending = (n1: number, n2: number) => n2 - n1;
 
-const sumAll = (arr: Array<number>): number => arr.reduce((acc, val) => acc + val, 0)
+const sum = (n1: number, n2: number) => n1 + n2
 
 export async function solutionPartOne() {
   return pipe(
@@ -30,8 +31,8 @@ export async function solutionPartOne() {
     flatMap(calculateTotalCaloriesForEachElf),
     sort(sortDescending),
     slice(0, 1),
-    sumAll
-  )(await readInput());
+    reduce(sum, 0)
+  )(await readInput())
 }
 solutionPartOne().then((a) => console.log('Question 1, Part 1', a));
 
@@ -41,8 +42,8 @@ export async function solutionPartTwo() {
     map(formatStringsIntoCollections),
     flatMap(calculateTotalCaloriesForEachElf),
     sort(sortDescending),
-    slice(0,3),
-    sumAll
-  )(await readInput());
+    slice(0, 3),
+    reduce(sum, 0)
+  )(await readInput())
 }
 solutionPartTwo().then((a) => console.log('Question 2, Part 2', a));
